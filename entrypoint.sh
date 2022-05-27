88e13ca049f39cbb502a2014313792a27bb15b75
Roman Pominov <rpominov+github@gmail.com>
2022-05-27 17:20:22 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 echo '=================== Create deploy key to push ==================='
 mkdir /root/.ssh
-ssh-keyscan -t rsa github.com > /root/.ssh/known_hosts && \
-ssh-keyscan -t rsa gitee.com >> /root/.ssh/known_hosts && \
-ssh-keyscan -t rsa codechina.csdn.net >> /root/.ssh/known_hosts && \
+ssh-keyscan -t rsa bitbucket.org > /root/.ssh/known_hosts && \
 echo "${GIT_DEPLOY_KEY}" > /root/.ssh/id_rsa && \
 chmod 400 /root/.ssh/id_rsa
 
 echo '=================== Sync with mirror git ==================='
-git version
-git fetch --unshallow origin
-git checkout master
-git status
-git remote add backup ${TARGET_GIT}
-git remote -vv
-git config --global --add safe.directory /github/workspace
-git config --global user.name "${GITHUB_ACTOR}"
-git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-echo 'ready to push'
-git push backup master -f
-git remote remove backup
+
+echo ${TARGET_GIT}
+
+# git version
+# git fetch --unshallow origin
+# git checkout master
+# git status
+# git remote add backup ${TARGET_GIT}
+# git remote -vv
+# git config --global --add safe.directory /github/workspace
+# git config --global user.name "${GITHUB_ACTOR}"
+# git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+# echo 'ready to push'
+# git push backup master -f
+# git remote remove backup
 
