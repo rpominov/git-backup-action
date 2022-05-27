12854ee3239b894429fbbe512fa2729e92e2e64c
Roman Pominov <rpominov+github@gmail.com>
2022-05-27 18:01:29 +0300

    update

--- Dockerfile
+++ Dockerfile
@@ -17,11 +17,11 @@ ENV LC_ALL C.UTF-8
 ENV LANG en_US.UTF-8
 ENV LANGUAGE en_US.UTF-8
 
-RUN apk add --no-cache \
-        git \
-        openssh-client \
-        libc6-compat \
-        libstdc++
+RUN apk add --no-cache git openssh-client 
+
+# \
+# libc6-compat \
+# libstdc++
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
+
+set -e
+
 echo '=================== SSH key ==========================='
+
 mkdir /root/.ssh
-ssh-keyscan -t rsa bitbucket.org > /root/.ssh/known_hosts && \
-echo "${GIT_DEPLOY_KEY}" > /root/.ssh/id_rsa && \
+ssh-keyscan -t rsa bitbucket.org > /root/.ssh/known_hosts
+echo "${GIT_DEPLOY_KEY}" > /root/.ssh/id_rsa
 chmod 400 /root/.ssh/id_rsa
 
 echo '=================== Git config ========================'
 
 git config --global --add safe.directory $PWD
-# git config --global user.name "${GITHUB_ACTOR}"
-# git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git remote add backup ${TARGET_GIT}
 git remote -v
 
@@ -22,4 +24,3 @@ git status
 echo '=================== Push =============================='
 
 git push backup master -f
-# git remote remove backup
