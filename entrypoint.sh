471cf88237b0397a189d1c193ed99f8e0a922a58
Roman Pominov <rpominov+github@gmail.com>
2022-05-31 16:30:12 +0300

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,25 @@
 
 set -e
 
-echo '=================== SSH key ==========================='
+# echo '=================== SSH key ==========================='
 
-mkdir /root/.ssh
-ssh-keyscan -t rsa bitbucket.org > /root/.ssh/known_hosts
-echo "${SSH_PRIVATE_KEY}" > /root/.ssh/id_rsa
-chmod 400 /root/.ssh/id_rsa
+# mkdir /root/.ssh
+# ssh-keyscan -t rsa bitbucket.org > /root/.ssh/known_hosts
+# echo "${SSH_PRIVATE_KEY}" > /root/.ssh/id_rsa
+# chmod 400 /root/.ssh/id_rsa
 
-echo '=================== Git config ========================'
 
-git config --global --add safe.directory $PWD
-git remote add backup ${REMOTE}
-git remote -v
+echo "${SSH_PRIVATE_KEY}" > /tmp_id_rsa
+GIT_SSH_COMMAND='ssh -o "StrictHostKeyChecking=no" -i /tmp_id_rsa' git push --repo="${REMOTE}" HEAD
+
+# FIXME: if push fails, this file remains, which might be a security concern?
+rm /tmp_id_rsa
+
+# echo '=================== Git config ========================'
+
+# git config --global --add safe.directory $PWD
+# git remote add backup ${REMOTE}
+# git remote -v
 
 # echo '=================== Fetch ============================='
 
@@ -23,4 +30,4 @@ git remote -v
 
 echo '=================== Push =============================='
 
-git push backup master
+# git push backup master
